870030ffa3af7d0c29d236516982901c4724a74f
Anshulrajkumar <anshulrajkumar777@gmail.com>
2026-02-09 15:25:03 +0530

    date of sch

---
+++ api/env.js
@@ -0,0 +1,12 @@
+module.exports = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Cache-Control", "no-store");
+
+  const supabaseUrl = process.env.SUPABASE_URL || "";
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+
+  res.status(200).json({
+    supabaseUrl,
+    supabaseAnonKey
+  });
+};
